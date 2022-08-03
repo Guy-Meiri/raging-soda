c546e84ee3b4e6856d70e39ff275afece84e13e6
guymeiri <guymeiri@microsoft.com>
2022-08-03 16:48:01 +0300

    added soda

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const playSodaSound = () => {
+    const sodaSound = new Audio("./soda-rage.mp3");
+    sodaSound.play();
+    console.log("playing soda sound");
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,60 +19,60 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
+        <Image src="/heart2.png" alt="Vercel Logo" height={72} width={72} />
+        <h1 className={styles.title}>Raging Soda</h1>
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          {"Because regaular soda just isn't as exiting"}
         </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+          <a
+            href="https://en.wikipedia.org/wiki/Carbonated_water"
+            className={styles.card}
+          >
             <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <p>Learn about the rich history of Soda water</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {/* <Image
+            className={styles.sodaImage}
+            onClick={playSodaSound}
+            src="/soda.png"
+            width={100}
+            height={100}
+            alt="soda"
+          /> */}
+          <div className={styles.sodaImage}>
+            <Image
+              onClick={playSodaSound}
+              src="/soda-faucet.jpeg"
+              width={150}
+              height={150}
+              alt="soda"
+            />
+          </div>
         </div>
+        {/* <Image
+          style={{ zIndex: -2, opacity: 0.12, pointerEvents: "none" }}
+          className={styles.sodaImage}
+          onClick={playSodaSound}
+          src="/soda-faucet.jpeg"
+          layout="fill"
+          objectFit="fill"
+          alt="soda"
+        /> */}
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://en.wikipedia.org/wiki/Carbonated_water"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Powered by cuteness
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
